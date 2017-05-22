fb1a6351f43394f79ec09ccad4ef26d337802b0a
U-BSD\456570 <456570@SHS-10DCE4369.beaverton.k12.or.us>
2017-05-22 14:25:05 -0700

    worked on

--- Graph.h
+++ Graph.h
@@ -7,10 +7,14 @@ using namespace std;
 class Graph{
  private:
   vector<Node*> Nodes;
+  Node* nodeSearch(char data);
   
  public:
-  
-
-
+  void addEdge(char data1, char data2, int newWeight);
+  void addVertex(char data);
+  void removeVertex(char data);
+  void removeEdge(char data1, char data2);
+  void print();
+  void findPath(char data1, char data2);
 
 };
--- Node.cpp
+++ Node.cpp
@@ -1,4 +1,22 @@
 #include <iostream>
 #include <cstring>
+#include "Node.h"
 
 using namespace std;
+
+Node::Node(char newName){
+  name = newName;
+}
+
+bool Node::remove(Node* toRemove){
+
+}
+
+bool Node::add(Node* toAdd, int newWeight){
+  
+}
+
+void Node::print(){
+
+
+}
